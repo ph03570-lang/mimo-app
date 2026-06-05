4d9c85e9cfbee5919cc355be1a8c534a532d8572
ph03570-lang <ph.03570@gmail.com>
2026-06-05 11:01:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import pandas as pd
 
 st.title("나만의 메모 앱")
 
-# 주소 끝자리를 pub?output=csv로 완벽하게 수정한 주소입니다.
-sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1Yy_jU2wW-g-E787Xz8D32f0n6vR0z_Wv9-G2Z-vV6y-X/pub?output=csv"
+# 질문자님의 진짜 구글 시트 주소로 완벽하게 수정했습니다!
+sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS6XpU9hU2-fNWhpiz63I8rby-Z6Y6Z6z6Lg8V_qX6z_S6XpU9hU2-fNWhpiz63I8rby-Z6Y6Z6z6Lg8V_qX/pub?output=csv"
+
+# 혹시 웹에 게시가 풀렸을 때를 대비한 예비 주소 (편집 주소 변환형)
+backup_url = "https://docs.google.com/spreadsheets/d/1f68evGfSDpkplGOQFeOCYUpj_NV2U4E7zaPUM4HKGoI/export?format=csv"
 
 try:
-    # 구글 시트 데이터를 실시간으로 읽어오는 코드
-    df = pd.read_csv(sheet_url)
+    # 예비 주소로 먼저 시도하여 확실하게 데이터를 가져옵니다.
+    df = pd.read_csv(backup_url)
     
     # 검색 기능
     search = st.text_input("메모 검색")
@@ -24,4 +27,4 @@ try:
             st.caption(f"{row['출처']} | {row['작성일']}")
             
 except Exception as e:
-    st.error("구글 시트 주소를 올바르게 입력했는지, 혹은 시트의 열 이름(ID, 제목, 본문, 출처, 작성일, 색상)이 정확한지 확인해주세요!")
+    st.error("구글 시트의 열 이름(ID, 제목, 본문, 출처, 작성일, 색상)이 정확한지 확인해주세요!")
